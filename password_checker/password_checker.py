bd1bef0a9e2af5c0bde826ec850b05254a09974f
brokoder <brokcoder@gmail.com>
2022-06-24 14:52:05 +0530

    project(1): linding changes added

--- password_checker/password_checker.py
+++ password_checker/password_checker.py
@@ -4,32 +4,47 @@ Instructions: use 'python password_checker.py <password>' or
 
 The password u enter will not be sent to the api only first 5 characters
 in the hashed sting is sent to the api.
-Api will respond back with all the compromised password hashes and is 
-matched with the hashed password you have entered to check if it is okey. 
+Api will respond back with all the compromised password hashes and is
+matched with the hashed password you have entered to check if it is okey.
 """
 import hashlib
-import requests
 import sys
+import requests
 
 
 class Password:
+    """
+    Class for defining the password attributes.
+    """
     def __init__(self, password: str) -> None:
         self.password = password
 
     def get_hashed_password(self):
+        """
+        Return the hashed string of password
+        """
         return hashlib.sha1(self.password.encode("utf-8")).hexdigest().upper()
 
     def get_query_strings(self):
+        """
+        Return the query string to be used as parameter in api.
+        """
         hashed_password = self.get_hashed_password()
         return hashed_password[:5], hashed_password[5:]
 
 
 class GetLeakedHashes:
+    """
+    Used to define the attributes of compromised password hashs
+    """
     def __init__(self, password_obj: Password) -> None:
         self.query_char, self.tail = password_obj.get_query_strings()
         self.password_used_count: int = 0
 
     def call_api_for_password(self):
+        """
+        For calling the pwnedpasswords api
+        """
         url = f"https://api.pwnedpasswords.com/range/{self.query_char}"
         res = requests.get(url)
         if res.status_code != 200:
@@ -39,6 +54,9 @@ class GetLeakedHashes:
         return res
 
     def check_if_password_is_okey(self):
+        """
+        For validating if the password is compromised
+        """
         api_response = self.call_api_for_password()
         hashes = {
             line.split(":")[0]: line.split(":")[1]
@@ -50,6 +68,9 @@ class GetLeakedHashes:
 
 
 def main(password):
+    """
+    Main function
+    """
     password_obj = Password(password)
     leaked_hashes = GetLeakedHashes(password_obj)
     leaked_hashes.check_if_password_is_okey()
@@ -61,5 +82,5 @@ def main(password):
 
 if __name__ == "__main__":
     args = sys.argv
-    password = args[1] if len(args) > 1 else input("Enter password:  ")
-    sys.exit(main(password))
+    password_input = args[1] if len(args) > 1 else input("Enter password:  ")
+    sys.exit(main(password_input))
