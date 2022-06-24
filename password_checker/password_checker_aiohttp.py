c3f588ba25c61bc418e206f2db2e296d78819264
brokoder <brokcoder@gmail.com>
2022-06-24 15:02:19 +0530

    project(2): linding changes added

--- password_checker/password_checker_aiohttp.py
+++ password_checker/password_checker_aiohttp.py
@@ -1,23 +1,27 @@
 """
 Instructions: use 'python password_async_checker.py -p "<passwords>"' or
-'python password_checker.py -l <location of the txt file with the passwords>' to enter your password.
+'python password_checker.py -l <location of the txt file with the passwords>'
+to enter your password.
 
 The password u enter will not be sent to the api only first 5 characters
 in the hashed sting is sent to the api.
-Api will respond back with all the compromised password hashes and is 
-matched with the hashed password you have entered to check if it is okey. 
+Api will respond back with all the compromised password hashes and is
+matched with the hashed password you have entered to check if it is okey.
 """
 
 import argparse
 import asyncio
-import aiohttp
 import hashlib
-import requests
+import aiohttp
 
 
 def start():
+    """
+    To be executed at the start
+    """
     parser = argparse.ArgumentParser(
-        description="This script is used for checking the list of passwords asynchronously. If the password is safe to use, then it will not be printed"
+        description="This script is used for checking the list of passwords asynchronously. \
+            If the password is safe to use, then it will not be printed"
     )
     parser.add_argument(
         "-p",
@@ -39,32 +43,21 @@ def start():
             passwords.extend(passwords_in_file)
     return passwords
 
-
-def http_get_sync(url: str):
-    response = requests.get(url)
-    return response
-
-
-async def http_get(url: str):
-    return await asyncio.to_thread(http_get_sync, url)
-
-
-async def call_api_for_password(password, hashed_pw):
-    res = await http_get(url)
-    if res.status_code != 200:
-        raise RuntimeError(f"Error fetching using api. status code{res.status_code}")
-    return password, res.text.splitlines()
-
-
 def get_tasks(session, format_passwords):
+    """
+    Getting the async tasks
+    """
     tasks = []
-    for password, hashed_pw in format_passwords.items():
+    for hashed_pw in format_passwords.values():
         url = f"https://api.pwnedpasswords.com/range/{hashed_pw[:5]}"
         tasks.append(asyncio.create_task(session.get(url, ssl=False)))
     return tasks
 
 
 async def main(passwords):
+    """
+    Main task
+    """
     format_passwords = {
         password: hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
         for password in passwords
@@ -80,13 +73,14 @@ async def main(passwords):
             }
             if format_passwords[password][5:] in hashes:
                 print(
-                    f"The password {password} is exposed {hashes[format_passwords[password][5:]]} times"
+                    f"The password {password} is exposed\
+                        {hashes[format_passwords[password][5:]]} times"
                 )
     return
 
 
 if __name__ == "__main__":
-    passwords: list = start()
-    if passwords == []:
+    passwords_input: list = start()
+    if passwords_input == []:
         raise EOFError("Please input the passwords to check")
-    asyncio.run(main(passwords))
+    asyncio.run(main(passwords_input))
